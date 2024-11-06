72d8b3cec63f3ef023fe7e4718436c97d9630a89
Nurali <izetkhannurali@gmail.com>
2024-11-06 14:28:07 +0500

    first

---
+++ tipa.js
@@ -0,0 +1,149 @@
+
+const img1 = document.getElementById('i_1');
+const img2 = document.getElementById('i_2');
+const img3 = document.getElementById('i_3');
+const img4 = document.getElementById('i_4');
+const img5 = document.getElementById('i_5');
+const img6 = document.getElementById('i_6');
+const img7 = document.getElementById('i_7');
+const img8 = document.getElementById('i_8');
+const img9 = document.getElementById('i_9');
+
+
+
+img1.addEventListener('mouseover', () => {
+   img1.src = './img/7.jpg'
+   img2.src = './img/9.jpg'
+   img3.src = './img/9.jpg'
+   img4.src = './img/5.jpg'
+   img5.src = './img/2.jpg'
+   img6.src = './img/2.jpg'
+   img7.src = './img/5.jpg'
+   img8.src = './img/2.jpg'
+   img9.src = './img/2.jpg'
+
+
+
+});
+
+img2.addEventListener('mouseover', () => {
+   img1.src = './img/8.jpg'
+   img2.src = './img/7.jpg'
+   img3.src = './img/9.jpg'
+   img4.src = './img/1.jpg'
+   img5.src = './img/5.jpg'
+   img6.src = './img/2.jpg'
+   img7.src = './img/1.jpg'
+   img8.src = './img/5.jpg'
+   img9.src = './img/2.jpg'
+
+
+
+});
+
+
+img3.addEventListener('mouseover', () => {
+   img1.src = './img/8.jpg'
+   img2.src = './img/8.jpg'
+   img3.src = './img/7.jpg'
+   img4.src = './img/1.jpg'
+   img5.src = './img/1.jpg'
+   img6.src = './img/5.jpg'
+   img7.src = './img/1.jpg'
+   img8.src = './img/1.jpg'
+   img9.src = './img/5.jpg'
+
+
+
+});
+
+img4.addEventListener('mouseover', () => {
+   img1.src = './img/6.jpg'
+   img2.src = './img/3.jpg'
+   img3.src = './img/3.jpg'
+   img4.src = './img/7.jpg'
+   img5.src = './img/9.jpg'
+   img6.src = './img/9.jpg'
+   img7.src = './img/5.jpg'
+   img8.src = './img/2.jpg'
+   img9.src = './img/2.jpg'
+
+
+
+});
+
+img5.addEventListener('mouseover', () => {
+   img1.src = './img/4.jpg'
+   img2.src = './img/6.jpg'
+   img3.src = './img/3.jpg'
+   img4.src = './img/8.jpg'
+   img5.src = './img/7.jpg'
+   img6.src = './img/9.jpg'
+   img7.src = './img/1.jpg'
+   img8.src = './img/5.jpg'
+   img9.src = './img/2.jpg'
+
+
+
+});
+
+
+img6.addEventListener('mouseover', () => {
+   img1.src = './img/4.jpg'
+   img2.src = './img/4.jpg'
+   img3.src = './img/6.jpg'
+   img4.src = './img/8.jpg'
+   img5.src = './img/8.jpg'
+   img6.src = './img/7.jpg'
+   img7.src = './img/1.jpg'
+   img8.src = './img/1.jpg'
+   img9.src = './img/5.jpg'
+
+
+
+});
+img7.addEventListener('mouseover', () => {
+   img1.src = './img/6.jpg'
+   img2.src = './img/3.jpg'
+   img3.src = './img/3.jpg'
+   img4.src = './img/6.jpg'
+   img5.src = './img/3.jpg'
+   img6.src = './img/3.jpg'
+   img7.src = './img/7.jpg'
+   img8.src = './img/9.jpg'
+   img9.src = './img/9.jpg'
+
+
+
+});
+
+
+img8.addEventListener('mouseover', () => {
+   img1.src = './img/4.jpg'
+   img2.src = './img/6.jpg'
+   img3.src = './img/3.jpg'
+   img4.src = './img/4.jpg'
+   img5.src = './img/6.jpg'
+   img6.src = './img/3.jpg'
+   img7.src = './img/8.jpg'
+   img8.src = './img/7.jpg'
+   img9.src = './img/9.jpg'
+
+
+
+});
+
+img9.addEventListener('mouseover', () => {
+   img1.src = './img/4.jpg'
+   img2.src = './img/4.jpg'
+   img3.src = './img/6.jpg'
+   img4.src = './img/4.jpg'
+   img5.src = './img/4.jpg'
+   img6.src = './img/6.jpg'
+   img7.src = './img/8.jpg'
+   img8.src = './img/8.jpg'
+   img9.src = './img/7.jpg'
+
+
+
+});
